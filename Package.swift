85d4c7948dc94dc895bbf590ced1b3f2e1e52d0a
Alex Moisei <dimentar@gmail.com>
2022-03-15 22:25:10 +0100

    Added Zendesk libs

--- Package.swift
+++ Package.swift
@@ -6,23 +6,78 @@ import PackageDescription
 let package = Package(
     name: "Zendesk-SP-iOS",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "Zendesk-SP-iOS",
-            targets: ["Zendesk-SP-iOS"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+            name: "ChatSDK",
+            targets: [
+                "ChatSDK",
+                "ChatProvidersSDK",
+                "MessagingSDK",
+            ]
+        ),
+        .library(
+            name: "ChatProvidersSDK",
+            targets: [
+                "ChatProvidersSDK",
+            ]
+        ),
+        .library(
+            name: "CommonUISDK",
+            targets: [
+                "CommonUISDK",
+            ]
+        ),
+        .library(
+            name: "MessagingSDK",
+            targets: [
+                "MessagingSDK",
+                "MessagingAPI",
+                "CommonUISDK",
+            ]
+        ),
+        .library(
+            name: "MessagingAPI",
+            targets: [
+                "MessagingAPI",
+                "SDKConfigurations",
+            ]
+        ),
+        .library(
+            name: "SDKConfigurations",
+            targets: [
+                "SDKConfigurations",
+            ]
+        ),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "Zendesk-SP-iOS",
-            dependencies: []),
-        .testTarget(
-            name: "Zendesk-SP-iOSTests",
-            dependencies: ["Zendesk-SP-iOS"]),
+        .binaryTarget(
+            name: "ChatSDK",
+            url: "https://github.com/zendesk/chat_sdk_ios/releases/download/2.11.2/ChatSDK.xcframework.zip",
+            checksum: "a72edead02928974be6050bcb7d026949e61a5925422a19a03bc0fedea369e8d"
+        ),
+        .binaryTarget(
+            name: "ChatProvidersSDK",
+            url: "https://github.com/zendesk/chat_providers_sdk_ios/releases/download/2.11.2/ChatProvidersSDK.xcframework.zip",
+            checksum: "348a45bd9ddea201d91f18159af3a4253af7c515acfd5bc9768d6ab294cfb2d7"
+        ),
+        .binaryTarget(
+            name: "CommonUISDK",
+            url: "https://github.com/zendesk/commonui_sdk_ios/releases/download/6.1.2/CommonUISDK.xcframework.zip",
+            checksum: "f74e8fe9772fd9fcaa4c26b2ad05cead75781a1cd0018c1e03b18ee00bf3a87a"
+        ),
+        .binaryTarget(
+            name: "MessagingSDK",
+            url: "https://github.com/zendesk/messaging_sdk_ios/releases/download/3.8.3/MessagingSDK.xcframework.zip",
+            checksum: "a0d6498844eecfb61a0003712fea0a9d205f838eef49dbccaaa5ba6ad9f5752c"
+        ),
+        .binaryTarget(
+            name: "MessagingAPI",
+            url: "https://github.com/zendesk/messagingapi_sdk_ios/releases/download/3.8.3/MessagingAPI.xcframework.zip",
+            checksum: "5b66c1122220ab45d59873b9243283186d5c0d9e196352fa66a062159f7e2c6a"
+        ),
+        .binaryTarget(
+            name: "SDKConfigurations",
+            url: "https://github.com/zendesk/sdkconfigurations_sdk_ios/releases/download/1.1.9/SDKConfigurations.xcframework.zip",
+            checksum: "1718f5cbf5c9737e12a30b5dca513975f7c707db9617cf9aaa870d4fed200ef5"
+        ),
     ]
 )
